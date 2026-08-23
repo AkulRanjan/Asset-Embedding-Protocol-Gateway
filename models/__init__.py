7ce6da3c7723404ae7acc0480934d95f40554bcb
Akul Ranjan <akulranjan06@gmail.com>
2026-08-23 15:15:18 +0530

    Update validation and attestation models

--- models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .attestation import Attestation, BlockResponse, Scores
-from .claim import Claim
-from .intent import Intent
-from .observation import Observation
-
-__all__ = ["Attestation", "BlockResponse", "Claim", "Intent", "Observation", "Scores"]
